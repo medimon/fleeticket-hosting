0c38b7e7aa6e8900d32103b6ba7883ca529eeda0
cmahdi <fouad-mehdi.hadjoudj@cmconsulting-dz.com>
2023-08-01 14:54:19 +0100

    switched to pg

--- fleeticket/settings.py
+++ fleeticket/settings.py
@@ -93,10 +93,14 @@ WSGI_APPLICATION = 'fleeticket.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qCBLJmarcTp3H26se2Y3',
+        'HOST': 'containers-us-west-200.railway.app',
+        'PORT': '6684',
+    } 
 }
 
 
